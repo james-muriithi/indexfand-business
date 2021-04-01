e8bf687e893558fd37053509dd8ea6bc53b74945
James Muriithi <muriithijames556@gmail.com>
2021-04-01 23:49:36 +0300

    poster tests

--- app/Http/Controllers/Admin/PosterController.php
+++ app/Http/Controllers/Admin/PosterController.php
@@ -6,9 +6,7 @@ use App\Http\Controllers\Controller;
 use App\Http\Helpers\exFPDF;
 use App\Models\Business;
 use Gate;
-use Illuminate\Http\Request;
-use SimpleSoftwareIO\QrCode\Facades\QrCode;
-use Symfony\Component\HttpFoundation\Response;
+use LaravelQRCode\Facades\QRCode;
 
 class PosterController extends Controller
 {
@@ -30,7 +28,7 @@ class PosterController extends Controller
         $pdf->AddFont('moonl','','Moon-Light.php');
 
         //logo
-        $pdf->image('images/logos/logo.png',10,2,15, 15);
+        $pdf->image('images/logos/logo.png',10,2,17, 17);
 
         //name
         $pdf->SetFont('moonb','',35);
@@ -41,9 +39,29 @@ class PosterController extends Controller
 
         //qr
         $url = "https://indexfand.com/pay/".$business->tag;
-        $qr = QrCode::format('png')->color(255, 0, 0)->generate($url);
-        return $qr;
-//        $pdf->image( $qr,$pdf->GetPageWidth()-50,25,-76);
+        $qr = QRCode::text($url)
+            ->setOutfile('images/qrs/'.$business->tag.'.png')
+            ->setSize(4)
+            ->png();
+
+        $pdf->image( 'images/qrs/'.$business->tag.'.png',$pdf->GetPageWidth()-50,23,-76);
+        //scan to pay
+        $pdf->SetFont('coves','',18);
+        $pdf->setXY($pdf->GetPageWidth()-27, $pdf->GetPageHeight() - 29);
+        $pdf->setTextColor(64, 64, 64);
+        $pdf->Cell(19,12,"Scan to pay",0,0,"R");
+
+        $pdf->setDrawColor(166, 166, 166);
+        $pdf->line(0,$pdf->GetPageHeight() - 15,$pdf->GetPageWidth() - 42,$pdf->GetPageHeight() - 15);
+//        $pdf->SetFillColor(0, 153, 0);
+//        $pdf->Rect(0, $pdf->GetPageHeight() - 15, $pdf->GetPageWidth(),15,'F');
+        $pdf->setTextColor(51, 51, 51);
+        $pdf->SetFont('bahaus','',22);
+        $pdf->SetXY(10,$pdf->GetPageHeight() - 14);
+        $pdf->Cell(19,13, ucwords($business->name),0,0, 'L');
+
+        //footer image
+        $pdf->image( 'images/logos/logo-footer.png',$pdf->GetPageWidth()-42,$pdf->GetPageHeight() - 15,40);
 
         $pdf->Output('D','test.pdf');
 
